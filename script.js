fab025ae52ea356266df4a29de978dfc92066dfb
Sandeep Raina <127074583+Sandeepraina40@users.noreply.github.com>
2025-07-28 16:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const modal = document.getElementById("myModal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close-modal");
+
+// Open modal on button click
+openBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+// Close modal on close button click
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Close modal if clicked outside modal-content
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
